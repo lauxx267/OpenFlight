12dfffad62839e3e9ab650e20bbe7a97f52eef60
Holly Newton <holnewt@users.noreply.github.com>
2015-10-14 15:45:15 -0500

    Create mission_interface.h
    This file was added to be compiled with the new main.c file.  The code is the same as mAEWing1-mas, except inti_mission and get_mission functions were added to make it compile with main.c

---
+++ Software/mission/mission_interface.h
@@ -0,0 +1,25 @@
+
+#ifndef MISSION_INTERFACE_H_
+#define MISSION_INTERFACE_H_
+
+extern void init_mission(
+		struct sensordata *sensorData_ptr,	///< pointer to sensorData structure
+		struct nav *navData_ptr,			///< pointer to navData structure
+		struct mission *missionData_ptr		///< pointer to missionData structure
+);
+
+extern void get_mission(
+		struct sensordata *sensorData_ptr,	///< pointer to sensorData structure
+		struct nav *navData_ptr,			///< pointer to navData structure
+		struct mission *missionData_ptr		///< pointer to missionData structure
+);
+
+
+/// Standard function to call the mission manager
+extern void run_mission(
+		struct sensordata *sensorData_ptr,	///< pointer to sensorData structure
+		struct nav *navData_ptr,			///< pointer to navData structure
+		struct mission *missionData_ptr		///< pointer to missionData structure
+);
+
+#endif /* MISSION_INTERFACE_H_ */
